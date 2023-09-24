f4dd2773aa91cdfc7eaf5a7e08088d7d8d72b360
RAGIRIKARTHIK <97691623+RAGIRIKARTHIK@users.noreply.github.com>
2023-09-25 02:29:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,1 @@
-// config.js
-module.exports = {
-    dbUri: 'mongodb+srv://karthikdellg15:hAE6TO167hZCkcY3@appexpo.7bfon78.mongodb.net/?retryWrites=true&w=majority',
-    jwtSecret: '123456789asdfghjkl',
-    cloudinary: {
-      cloud_name: 'dkewx4sgu',
-      api_key: '325439813469475',
-      api_secret: 'eYv_ZyAYmx8fF7-hIPWLHKusY18',
-    }
-  };
-  
+
